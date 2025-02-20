346eea0fb7f8ed007320bf43d7647e3c65161b7a
Lenize Ramos <lenizemr@gmail.com>
2025-02-20 13:00:49 -0800

    add dots on the background

---
+++ src/app/components/AnimatedBackground.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+
+const AnimatedBackground: React.FC = () => {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    const ctx = canvas.getContext("2d");
+    if (!ctx) return;
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    const points = Array.from({ length: 1000 }).map(() => ({
+      x: Math.random() * canvas.width,
+      y: Math.random() * canvas.height,
+      radius: Math.random() * 3 + 3,
+      dx: (Math.random() - 0.5) * 3,
+      dy: (Math.random() - 0.5) * 3,
+    }));
+
+    const animate = () => {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+      points.forEach((point) => {
+        ctx.beginPath();
+        ctx.arc(point.x, point.y, point.radius, 0, Math.PI * 2);
+        ctx.fillStyle = "#ff007f";
+        ctx.fill();
+
+        point.x += point.dx;
+        point.y += point.dy;
+
+        if (point.x > canvas.width || point.x < 0) point.dx = -point.dx;
+        if (point.y > canvas.height || point.y < 0) point.dy = -point.dy;
+      });
+
+      requestAnimationFrame(animate);
+    };
+
+    animate();
+
+    const handleResize = () => {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <canvas
+      ref={canvasRef}
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        zIndex: -1,
+        width: "100%",
+        height: "100%",
+      }}
+    />
+  );
+};
+
+export default AnimatedBackground;
--- src/app/components/SectionWrapper.tsx
+++ src/app/components/SectionWrapper.tsx
@@ -9,7 +9,7 @@ const SectionWrapper: React.FC<SectionWrapperProps> = ({ id, children }) => {
   return (
     <section
       id={id}
-      className="min-h-screen w-full flex flex-col justify-center items-center bg-[linear-gradient(to_top,_var(--bgColorStart),_var(--bgColorEnd))] p-8"
+      className="min-h-screen w-full flex flex-col justify-center items-center bg-[linear-gradient(to_top,_var(--bgColorStart),_var(--bgColorEnd))] p-8 z-[-2]"
     >
       {children}
     </section>
